4eda8b5e3b3fbd40027342207d8bfc8bc50929a0
Ajanthan <ajanthanasia@gmail.com>
2024-04-07 16:10:21 +0530

    play button ok

--- music/src/pages/home/home.js
+++ music/src/pages/home/home.js
@@ -7,20 +7,18 @@ import { AiFillPlayCircle, AiFillPauseCircle } from "react-icons/ai";
 import mySong from "C:/xampp/htdocs/github/mission/music/src/songs/Katchi-Sera.mp3";
 export default function Home() {
   const [isPlaying, setIsPlaying] = useState(false);
-  const audio = new Audio(mySong);
+  // const audio = new Audio(mySong);
+  const [audio] = useState(new Audio(mySong));
 
   const toggleAudio = () => {
-    console.log(isPlaying);
-    if (isPlaying) {
-      console.log(audio.pause());
+    if (isPlaying && audio) {
       audio.pause();
-    } else {
-      console.log(audio.play());
+      setIsPlaying(false);
+    } else if (!isPlaying && audio) {
       audio.play();
+      setIsPlaying(true);
     }
-    setIsPlaying(!isPlaying);
   };
-
   function SongView() {
     return (
       <div className="playlist-card">
@@ -45,20 +43,6 @@ export default function Home() {
       <div className="ScreenContainer">
         <img src="" alt="" />
         <div className="library-body">
-          {/* <div className="playlist-card">
-            <img
-              src="https://as1.ftcdn.net/v2/jpg/02/23/60/54/1000_F_223605406_nGKtPp42ZRx4ZxvrcVeT3Ek6V5Uw4ETh.jpg"
-              className="playlist-image"
-              alt="Playlist-Art"
-            />
-            <p className="playlist-title">Playlist title</p>
-            <p className="playlist-subtitle">playlist subtitle</p>
-            <div className="playlist-fade" onClick={toggleAudio}>
-              <IconContext.Provider value={{ size: "50px", color: "#E99D72" }}>
-                {isPlaying ? <AiFillPauseCircle /> : <AiFillPlayCircle />}
-              </IconContext.Provider>
-            </div>
-          </div> */}
           <SongView />
           <SongView />
           <SongView />
